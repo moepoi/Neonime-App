f7ecc1fc7f10728a9f14292495aca792b34091dc
Moe <moepoi@protonmail.com>
2020-08-18 01:37:58 +0700

    Update app config source

--- lib/components/initializer.dart
+++ lib/components/initializer.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 final appVersion = '1.0.0';
 
 dynamic initializer() async {
-  final urlConfig = 'https://moepoi.dev/test.json';
+  final urlConfig = 'https://gitlab.com/moepoi/neonime-app/-/raw/master/info.json';
   final response = await http.get(urlConfig);
   var res = json.decode(response.body);
   return res;
